b844b5866b849acc61039a510457ce823bf2425c
James Coleman <colema18@gmail.com>
2025-07-27 20:05:10 -0500

    update config reference

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import * as k8s from "@pulumi/kubernetes";
 import ServiceDeployment from "./component-resources/service-deployment";
 
 const config = new pulumi.Config();
-const helloPulumiUiImageTag = config.require("iac-application:helloPulumiUiImageTag");
-const helloPulumiAppImageTag = config.require("iac-application:helloPulumiAppImageTag");
+const helloPulumiUiImageTag = config.require("helloPulumiUiImageTag");
+const helloPulumiAppImageTag = config.require("helloPulumiAppImageTag");
 
 
 
